ef33049314358793fb7d7585338b1f6960b38d84
李雪洋 <lixueyang@qianmi.com>
2017-10-10 16:27:28 +0800

    提交

---
+++ 83.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2017/10/10 0010 下午 12:43
+# @Author  : 李雪洋
+# @File    : 83.py
+# @Software: PyCharm
+
+import re
+from urllib import request
+from urllib import parse
+import json
+
+try:
+
+    url = 'http://fanyi.baidu.com/sug'
+    formData = {}
+    formData['kw'] = '中国'
+
+    param = parse.urlencode(formData).encode('utf-8')
+
+    res = request.urlopen(url, param)
+
+    if res.getcode() != 200:
+        print("页面返回失败")
+    data = res.read().decode('unicode_escape')
+
+    jsonObj = json.loads(data)
+
+    for i in jsonObj['data']:
+        print(i['k'] + " : " + i['v'])
+
+except Exception as e:
+    print(e)
+finally:
+    print("结束")
